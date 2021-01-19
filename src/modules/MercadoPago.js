be249cee4563d6aa4a41116764ebb6c1230ac6e4
Sasuke <davidkunsch3@gmail.com>
2021-01-19 16:57:54 -0300

    now works

--- src/modules/MercadoPago.js
+++ src/modules/MercadoPago.js
@@ -7,14 +7,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 module.exports = (client) => {
-    app.post('/payments', (req, res) => {
-        console.log(req.body);
-        res.sendStatus(200);
-    }); 
-
-    app.get('/payments', (req, res) => {
-        res.sendStatus(200);
-    })
+    app.get('/', (req, res) => {
+        console.log(req.query, req.params);
+        res.sendStatus(201);
+    });
 
     app.listen(port, () => {
         client.log('Express connected! Listenening at port ' + port, { color: 'green', tags: [ 'MercadoPago' ] });
